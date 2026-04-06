fc882516fb88b9ff7f5f0cf320061152d6e2f121
K-AVLB <95263567+K-AVLB@users.noreply.github.com>
2026-04-06 10:39:30 +0900

    Use session storage for admin auth

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://oaiukptspaedferkqzqd.supabase.co';
 const supabaseKey = 'sb_publishable_8kk_fuqNm_-uqmEdn5xQag_QK4j1YSE';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const authOptions =
+  typeof window === 'undefined'
+    ? {}
+    : {
+        persistSession: true,
+        storage: window.sessionStorage,
+      };
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: authOptions,
+});
